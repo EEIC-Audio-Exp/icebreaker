b4973bfa324f4abb21f02d73a5757c1e37eee727
Daiki Kawahara <kawahara-daiki542@g.ecc.u-tokyo.ac.jp>
2024-12-12 14:59:58 +0900

    feat: 自己紹介中の音声をtrainように保存するコードを追加

---
+++ introduction/make_train_data.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# 
+# 
+
+## dialogue directory ##
+tmpdirname=./train_data
+
+#話者番号
+spkid=$1
+
+#ファイル番号
+file_num=1
+
+# もしディレクトリが存在しなければ作成
+if [ ! -e $tmpdirname ];then
+	mkdir ${tmpdirname}
+fi
+
+while true; do
+	# adinrec による録音
+	filename=${tmpdirname}/wav/s${spkid}_$(printf "%03d" $file_num).wav
+	padsp adinrec $filename > /dev/null
+	# Ctrl-C で抜けるための処理
+	if [ ! -e $filename ];then
+		rmdir $tmpdirname
+		exit;
+	fi
+
+    #事後処理
+    #現在は、10件保存したらループを抜けるようにしている
+
+    echo -e "/home/denjo/icebreaker/introduction/$filename\t$spkid" >> ./train_data/wav-spklist.txt
+    if [ "$file_num" -eq 10 ];then
+        break
+    fi
+    file_num=$((file_num + 1))
+
+done
