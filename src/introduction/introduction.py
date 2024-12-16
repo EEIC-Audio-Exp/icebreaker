4dee486a7a72d0abce20112cc6156341925a4ad1
Daiki Kawahara <kawahara-daiki542@g.ecc.u-tokyo.ac.jp>
2024-12-16 12:56:56 +0900

    ファイルの分割、音声による誘導の追加

--- src/introduction/introduction.py
+++ src/introduction/introduction.py
@@ -1,6 +1,6 @@
-import os
-import subprocess
 from introduction.ask_number import ask_number
+from introduction.record import record
+from introduction.train import train
 
 def introduction():
     # 自己紹介のメイン処理
@@ -10,24 +10,11 @@ def introduction():
     # -> 参加人数の音声から人数を取得
     spk_num = ask_number() 
     
-    #音声ファイルリストを初期化
-    spklist_path = "./introduction/train_data/wav-spklist.txt"
-    if os.path.isfile(spklist_path):
-        with open(spklist_path,"w") as f:
-            pass
-    
-    make_train_data_path = "./introduction/make_train_data.sh"
-    for spk_id in range(1, spk_num+1):
-        # -> チームメンバーの自己紹介を促す音声を流す
-        print(str(spk_id) + "人目の人は自己紹介をはじめてください。")
-        # -> 各メンバーの音声を保存
-        res = subprocess.run(["bash", make_train_data_path,str(spk_id)], capture_output=True, text=True)
+    # 順番に自己紹介
+    # 自己紹介の音声を録音する
+    record(spk_num)
 
-    #全員の音声をもとに学習
-    train_path = "../dialogue-demo/sid/train.sh"
-    res = subprocess.run(["bash", train_path, spklist_path], capture_output=True, text=True)
-
-    #自己紹介の終了
-    print("これで自己紹介は終わりです。ありがとうございました。")
+    # 録音された音声を使って学習を行う
+    train()
     pass
 
---
+++ src/introduction/train.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# coding: utf-8
+#
+# 録音データをもとに学習を行う
+
+import sys, os
+import subprocess
+import re
+from multiprocessing import Process
+
+jtalkbin = 'open_jtalk '
+#options = '-m /usr/share/hts-voice/nitech-jp-atr503-m001/nitech_jp_atr503_m001.htsvoice -ow /tmp/dialogue/out.wav -x /var/lib/mecab/dic/open-jtalk/naist-jdic'
+options = '-m ../Voice/mei/mei_angry.htsvoice -ow ./tmp_out.wav -x /var/lib/mecab/dic/open-jtalk/naist-jdic -r 1.0 -fm 1.0'
+
+
+# 音声合成のコマンドを生成 (open jtalk を 使う場合)
+def mk_jtalk_command(answer):
+    jtalk = 'echo "' + answer + '" | ' + jtalkbin + options + ';'
+    play = 'play -q ./tmp_out.wav; rm ./tmp_out.wav;'
+    return jtalk + play
+
+def task_train():
+    train_path = "../dialogue-demo/sid/train.sh"
+    spklist_path = "./introduction/train_data/wav-spklist.txt"
+    res = subprocess.run(["bash", train_path, spklist_path], capture_output=True, text=True)
+
+def task_jtalk():
+    end_intro = "自己紹介が終わったので、ここからアイスブレイキングに入ります。"
+    os.system(mk_jtalk_command(end_intro))
+
+def train():
+    #全員の音声をもとに学習しつつ、その間を音声でもたせる
+    process_train = Process(target=task_train)
+    process_jtalk = Process(target=task_jtalk)
+
+    process_train.start()
+    process_jtalk.start()
+
+    process_train.join()
+    process_jtalk.join()
+    return
+
+if __name__ == '__main__':
+    train()
