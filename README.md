fe068169dc4f854468d53dc10c4adf6246ab891c
Omusubi0123 <Omusubi0123@users.noreply.github.com>
2024-12-17 15:11:31 +0900

    feat: 緊張度から次の話題を選択

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ sudo apt update
 sudo apt install python3 python3-pip
 pip install --upgrade pip
 pip install openai-whisper
+pip install -r ./requiments.txt
 sudo apt install ffmpeg
--- src/icebreak/propose_topic.py
+++ src/icebreak/propose_topic.py
@@ -6,13 +6,21 @@ import pandas as pd
 sys.path.append('..')
 from dynamic_manager import dynamic_value_manager
 from .speak import mk_jtalk_command
+from .get_emotion.get_tension  import get_tension
 
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
-topics = pd.read_csv(os.path.join(script_dir, 'topics.csv'))['topic'].tolist()
+topic_df = pd.read_csv(os.path.join(script_dir, 'topics.csv'))
+
+def determine_topic() -> str:
+    tension = get_tension('./input.wav')
+    comical = 100 - tension
+    nearest_index = (df['tension'] - comical).abs().idxmin()
+    nearest_topic = df.loc[nearest_index, 'topic']
+    return nearest_topic
 
 def propose_topic():
-    topic = random.choice(topics)
+    topic = determine_topic()
     dynamic_value_manager.set_value(topic)
     speak_str = f"では、次は{topic} について話しましょう！"
     os.system(mk_jtalk_command(speak_str))
